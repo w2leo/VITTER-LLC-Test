ce60672c7b1e1f9e971db2ae375a7b3b564c841c
w2leo <82013108+w2leo@users.noreply.github.com>
2022-10-13 16:36:13 +0300

    fixed GameCube
    fixed problem with Z coordinate
    fixed SetPosition()

--- Assets/Scripts/GameCube.cs
+++ Assets/Scripts/GameCube.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 
 public class GameCube : MonoBehaviour
 {
-    private const float baseZ = 1.0f;
+    private const float baseZ = -1.0f;
 
     [SerializeField] private Collider2D collider2D;
-    
+
     private bool canMove = false;
 
-    public bool CanMove { get => canMove; }    
-    
+    public bool CanMove { get => canMove; }
+
     public void ChangeMoveState(bool state)
     {
         canMove = state;
@@ -23,6 +23,6 @@ public class GameCube : MonoBehaviour
 
     public void SetPosition(float x, float y)
     {
-        Vector3 newPosition = new Vector3(x, y, baseZ);
+        transform.position = new Vector3(x, y, baseZ);
     }
 }
