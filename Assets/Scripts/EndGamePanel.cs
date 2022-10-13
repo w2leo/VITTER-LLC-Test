147929ee0032a92fc1ceb822879ab08410f15060
w2leo <82013108+w2leo@users.noreply.github.com>
2022-10-13 14:39:19 +0300

    visual fixes
    1. Close button endGamePanel deactivated
    2. Sound panel deactivates on ButtonClose click

--- Assets/Scripts/EndGamePanel.cs
+++ Assets/Scripts/EndGamePanel.cs
@@ -8,7 +8,8 @@ public class EndGamePanel : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI text;
     [SerializeField] Transform menuPanels;
-
+    [SerializeField] Button buttonClose;
+ 
     private const string winState = "œŒ¡≈ƒ¿";
     private const string looseState = "œŒ–¿∆≈Õ»≈";
 
@@ -16,6 +17,7 @@ public class EndGamePanel : MonoBehaviour
     {
         menuPanels.gameObject.SetActive(true);
         gameObject.SetActive(true);
+        buttonClose.gameObject.SetActive(false);
         if (state)
         {
             text.text = winState;
